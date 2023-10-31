06b605435f04170cd831d6d622d0fcfe25c44d50
Andrei Korikov <kpanic666@gmail.com>
2023-10-31 14:13:24 -0400

    final version

--- Hangman (Python)/task/hangman/hangman.py
+++ Hangman (Python)/task/hangman/hangman.py
@@ -3,13 +3,19 @@ import string
 
 MAX_ATTEMPTS = 8
 GAME_FIELD_MASK = "-"
+USER_COMMAND_PLAY = "play"
+USER_COMMAND_RESULTS = "results"
+USER_COMMAND_EXIT = "exit"
+USER_HINT = 'Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:'
+GS_WON_KEY = "You won"
+GS_LOST_KEY = "You lost"
 
 words_list = frozenset({'python', 'java', 'swift', 'javascript'})
-guessed_word = random.choice(list(words_list))
+guessed_word = ""
 guessed_letters = set()
-left_attempts = MAX_ATTEMPTS
-game_field = list(GAME_FIELD_MASK * len(guessed_word))
-win_flag = False
+left_attempts = 0
+game_field = list()
+game_statistics = {GS_WON_KEY: 0, GS_LOST_KEY: 0}
 
 
 def update_game_field(letter):
@@ -53,17 +59,35 @@ def check_user_input(user_input) -> bool:
 
 print("H A N G M A N")
 
-while left_attempts > 0 and not check_win_conditions(game_field):
-    print_game_field(game_field)
-    user_letter = input("Input a letter: ")
-    if not check_user_input(user_letter):
-        continue
-    update_game_field(user_letter)
+while True:
+    game_mode = input(USER_HINT)
 
-if check_win_conditions(game_field):
-    print_game_field(game_field)
-    print("You guessed the word " + "".join(guessed_word) + "!")
-    print("You survived!")
-else:
-    print()
-    print("You lost!")
+    if game_mode == USER_COMMAND_EXIT:
+        break
+    elif game_mode == USER_COMMAND_RESULTS:
+        print(GS_WON_KEY + ": " + str(game_statistics[GS_WON_KEY]) + " times.")
+        print(GS_LOST_KEY + ": " + str(game_statistics[GS_LOST_KEY]) + " times.")
+    elif game_mode == USER_COMMAND_PLAY:
+        left_attempts = MAX_ATTEMPTS
+        guessed_word = random.choice(list(words_list))
+        guessed_letters.clear()
+        game_field = list(GAME_FIELD_MASK * len(guessed_word))
+        while left_attempts > 0 and not check_win_conditions(game_field):
+            print_game_field(game_field)
+            user_letter = input("Input a letter: ")
+            if not check_user_input(user_letter):
+                continue
+            update_game_field(user_letter)
+
+        if check_win_conditions(game_field):
+            print_game_field(game_field)
+            print("You guessed the word " + "".join(guessed_word) + "!")
+            print("You survived!")
+            game_statistics[GS_WON_KEY] += 1
+        else:
+            print()
+            print("You lost!")
+            game_statistics[GS_LOST_KEY] += 1
+
+
+
